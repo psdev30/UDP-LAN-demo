447acde3b9e19b27d2c8fa178c5acd1e011f72c2
psdev30 <psdev3012@gmail.com>
2022-11-17 17:03:05 -0500

    stuff

--- src/echo_client.rs
+++ src/echo_client.rs
@@ -6,29 +6,33 @@ use std::str::from_utf8;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Need to specify host to connect to");
+    // if hostname isn't entered exit 
+    while args.len() < 2 {
+        println!("Need to specify host to connect to. Try again");
         std::process::exit(1);
     }
 
     let socket = UdpSocket::bind("127.0.0.1:9800").expect("Could not connect to 127.0.0.1:9800");
     println!("Successfully binded host to 127.0.0.1:9800!");
+    println!("You may start sending messages to the host below:");
 
+    // gets host addr arg
     let hostname = &args[1];
 
+    // connects to the host on the entered addr + port 8080
     socket.connect(hostname.to_string() + &":8080").expect("Could not connect to 127.0.0.1:8080");
 
+    // gets msg from stdin that we would like to send to the host
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
-        //println!("Line read from stdin '{}'", line);
         // "k" kills input reading
-        if &line == "k" {
-            break;
-        }
+        if &line == "k" { break; }
 
+        // sends msg to host. can use send() bc we explicitly connected to the host address/port
         socket.send(line.as_bytes()).expect("Error on send");
 
+        // receives echo of input from server and prints as confirmation
         let mut buf = [0; 2048];
         let (amt, _src) = socket.recv_from(&mut buf).expect("didn't receive the expected data :(");
         let echo = from_utf8(&buf[..amt]).unwrap();
